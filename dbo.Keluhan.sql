2f1388c728d3211c738d9c510320a538bd2d54de
Rio Surya Fachrudin <32932036+riosurya12@users.noreply.github.com>
2022-08-12 22:11:58 +0700

    Add files via upload

---
+++ dbo.Keluhan.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Keluhan] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [Keluhanmu] NVARCHAR (MAX) NULL,
+    [Solusimu]  NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Keluhan] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
